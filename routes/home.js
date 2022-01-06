514c3136f3c48b52bed71b4cec454fc5b6fdb994
Herr-Hauptmann <tarik.horoz@gmail.com>
2022-01-06 17:36:53 +0100

    uradjen zahtjev get vjezbe

--- routes/home.js
+++ routes/home.js
@@ -1,29 +1,22 @@
 const express= require('express')
 const router = express.Router();
+const fs = require('fs');
 
 router.get('/', (req, res, next) =>{
     res.setHeader('Content-Type', 'text/html');
     res.render('z1')
 });
 
-router.get('/z1', (req, res, next) =>{
-    res.setHeader('Content-Type', 'text/html');
-    res.render('z1')
-});
-
-router.get('/vjezbe', (req,res,next) =>{
-    res.setHeader('Content-Type', 'text/html');
-    res.render('vjezbe')
-});
-
-router.get('/zadaci', (req,res,next) =>{
-    res.setHeader('Content-Type', 'text/html');
-    res.render('zadaci')
-});
-
-router.get('/mojRepozitorij', (req,res,next) =>{
-    res.setHeader('Content-Type', 'text/html');
-    res.render('mojRepozitorij')
+router.get('/vjezbe', (req, res, next)=>{
+    let podaci = fs.readFileSync('public/vjezbe.csv','utf-8').split(',');
+    let odgovor = {brojVjezbi: podaci[0], brojZadataka:[]};
+    for (let i = 1; i <= odgovor.brojVjezbi; i++)
+    {
+        odgovor.brojZadataka.push(podaci[i]);
+    }
+    res.setHeader('content-type', 'application/json');
+    res.write(JSON.stringify(odgovor));
+    res.end();
 });
 
 module.exports = router;
