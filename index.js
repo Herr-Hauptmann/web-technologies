ec554a5f38e00c541fa9c1d38599207ff130df56
Herr-Hauptmann <tarik.horoz@gmail.com>
2022-01-10 20:56:13 +0100

    implementiran post na /vjezbe

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const rute = require('./routes/home')
 const ruteHTML = require('./routes/irfan')
 
@@ -8,8 +7,6 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'public/html');
 app.use(express.static(__dirname + '/public'));
-
-app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(rute);
 app.use(ruteHTML);
--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,11 @@
 const express= require('express')
 const router = express.Router();
 const fs = require('fs');
+const bodyParser = require('body-parser');
+const { fileURLToPath } = require('url');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var jsonParser = bodyParser.json(); 
 
 router.get('/', (req, res, next) =>{
     res.setHeader('Content-Type', 'text/html');
@@ -16,6 +21,42 @@ router.get('/vjezbe', (req, res, next)=>{
     }
     res.setHeader('content-type', 'application/json');
     res.write(JSON.stringify(odgovor));
+    res.end();
+});
+
+router.post('/vjezbe', jsonParser, (req, res, next)=>{
+    res.setHeader('content-type', 'application/json');
+    let greske = '';
+    let string = `${req.body.brojVjezbi}`;
+    if (req.body.brojVjezbi <= 0 || req.body.brojVjezbi > 15)
+    {
+        greske+='brojVjezbi';
+    }
+    if (req.body.brojZadataka.length != req.body.brojVjezbi)
+    {
+        greske+=(greske)?',':'';
+        greske+='brojZadataka';
+    }
+    let i = 0;
+    for (const n of req.body.brojZadataka)
+    {
+        if (n < 0 || n>10)
+        {
+            greske+=(greske)?',':'';
+            greske+=`n${i}`; 
+        }
+        string+=`,${n}`;
+        i++;
+    }
+    if (greske != ''){
+        res.write(JSON.stringify({"status": 'error', 'data' : `Pogresan parametar ${greske}`}));
+        res.end();
+        return;
+    }
+    
+    fs.truncateSync('public/vjezbe.csv');
+    fs.writeFileSync('public/vjezbe.csv', string);
+    res.write(JSON.stringify(req.body));
     res.end();
 });
 
