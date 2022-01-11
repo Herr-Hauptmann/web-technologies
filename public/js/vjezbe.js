46db419d6f82630963656c9ceb41d34e77619f51
Herr-Hauptmann <tarik.horoz@gmail.com>
2022-01-11 14:19:22 +0100

    gotovo crtanje vjezbi

---
+++ public/js/vjezbe.js
@@ -0,0 +1,3 @@
+let vjezbe = document.getElementById('odabirVjezbe');
+console.log('radi');
+VjezbeAjax.iscrtajVjezbe(vjezbe, {brojVjezbi: 12, brojZadataka:[1,2,3]});
--- public/js/vjezbeAjax.js
+++ public/js/vjezbeAjax.js
@@ -25,6 +25,7 @@ let VjezbeAjax = (function(){
             DOMelementDIVauFormi.appendChild(div);
         }
     }
+
     let posaljiPodatke = function(vjezbeObjekat,callbackFja){
         var ajax = new XMLHttpRequest();
         ajax.open("POST","http://localhost:3000/vjezbe",true);
@@ -43,11 +44,90 @@ let VjezbeAjax = (function(){
                 callbackFja(ajax.statusText,null);
         }
 
-        ajax.send(JSON.stringify(vjezbeObjekat));
+        ajax.send();
+    }
+
+    let dohvatiPodatke = function(callbackFja){
+        var ajax = new XMLHttpRequest();
+        ajax.open("GET","http://localhost:3000/vjezbe",true);
+        ajax.setRequestHeader("Content-Type", "application/json");
+        ajax.onreadystatechange = function() {
+            if (ajax.readyState == 4 && ajax.status == 200){
+                var rezultat = JSON.parse(ajax.responseText);
+                if (rezultat.status=="error")
+                {
+                    callbackFja(rezultat.data, null);
+                }
+                callbackFja(null, rezultat);
+            }
+            else if (ajax.readyState == 4)
+                callbackFja(ajax.statusText,null);
+        }
+        ajax.send();
+    }
+
+    iscrtajVjezbe = function(divDOMelement,objekat){
+        for (let i = 0; i < objekat.brojVjezbi; i++)
+        {
+            let div = document.createElement('div');
+            div.classList.add("card");
+            div.classList.add("border-secondary");
+            div.appendChild(iscrtajNaslovVjezbe(i+1));
+            divDOMelement.appendChild(div);
+        }
+    };
+
+    iscrtajZadatke = function(vjezbaDOMelement,brojZadataka){
+        // <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#odabirVjezbe">
+        //                 <div class="card-body text-secondary">
+        //                     <button type="button" class="btn btn-outline-dark my-2">Zadatak 1</button>
+        //                     <button type="button" class="btn btn-outline-dark my-2">Zadatak 2</button>
+        //                     <button type="button" class="btn btn-outline-dark my-2">Zadatak 3</button>
+        //                     <button type="button" class="btn btn-outline-dark my-2">Zadatak 4</button>
+        //                 </div>
+        //               </div>
+    };
+
+    iscrtajNaslovVjezbe = function(brojVjezbe){
+        div = document.createElement("div");
+        div.classList.add('card-header');
+        div.id=`heading${brojVjezbe}`;
+        
+        h2 = document.createElement("h2");
+        h2.classList.add('mb-0');
+        
+        
+        button = document.createElement('button');
+        button.classList.add('btn');
+        button.classList.add('btn-link');
+        button.classList.add('btn-block');
+        button.classList.add('text-dark');
+        button.classList.add('text-left');
+        button.type = "button";
+        button.dataset.toggle="collapse";
+        button.dataset.target=`#collapse${brojVjezbe}`;
+        button.setAttribute("aria-expanded", "true");
+        button.setAttribute('aria-controls', `collapse${brojVjezbe}`);
+        button.innerHTML = `Vježba ${brojVjezbe}`;
+        
+        h2.appendChild(button);
+        div.appendChild(h2);
+        // <div class="card-header" id="headingOne">
+        //     <h2 class="mb-0">
+        //         <button class="btn btn-link btn-block text-left " type="button" 
+        //         data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        //         Vježba 1
+        //         </button>
+        //     </h2>
+        // </div>
+        return div;
     }
 
     return{
         posaljiPodatke: posaljiPodatke, 
-        dodajInputPolja:dodajInputPolja
+        dodajInputPolja: dodajInputPolja,
+        dohvatiPodatke: dohvatiPodatke,
+        iscrtajVjezbe: iscrtajVjezbe,
+        iscrtajZadatke: iscrtajZadatke
     };
 }());
