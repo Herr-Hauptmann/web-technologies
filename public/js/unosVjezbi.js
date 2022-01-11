0244a77c15fb0d80796d5dd4afaaa6789f0a9cfc
Herr-Hauptmann <tarik.horoz@gmail.com>
2022-01-11 12:59:19 +0100

    gotov zadatak 1

--- public/js/unosVjezbi.js
+++ public/js/unosVjezbi.js
@@ -1,4 +1,23 @@
 let unosBrojaVjezbi = document.getElementById('brojVjezbi');
 let poljeZaVjezbe = document.getElementById('unosZadataka');
+let submitDugme = document.getElementById('posalji');
 unosBrojaVjezbi.addEventListener('change', ()=>{VjezbeAjax.dodajInputPolja(poljeZaVjezbe, unosBrojaVjezbi.value)});
+submitDugme.addEventListener('click', ()=>{
+    let objekat = {
+        brojVjezbi: unosBrojaVjezbi.value,
+        brojZadataka: []
+    };
+    let zadaci = document.getElementsByClassName('zadatak');
+    for (let i = 0; i < zadaci.length; i++)
+    {
+        objekat.brojZadataka.push(parseInt(zadaci[i].value));
+    }
+    VjezbeAjax.posaljiPodatke(objekat, ispisi);
+});
 
+function ispisi(error, data){
+    if (error!=null)
+        alert(`Greska: ${error}`);
+    else
+        alert(`Promjena uspjesna! Uneseno ${data.brojVjezbi} vjezbi!`);
+}
--- public/js/vjezbeAjax.js
+++ public/js/vjezbeAjax.js
@@ -1,20 +1,23 @@
 let VjezbeAjax = (function(){
     let dodajInputPolja = function(DOMelementDIVauFormi,brojVjezbi){
+        DOMelementDIVauFormi.innerHTML='';
         for (i=0;i<brojVjezbi;i++){
-            // Create an <input> element, set its type and name attributes
             var labela = document.createElement("label");
             labela.classList.add('col-md-4');
             labela.classList.add('col-form-label');
             labela.classList.add('mt-2');
-            labela.innerHTML=`Broj zadataka vježbe ${i+1}`;
-            labela.for='vjezba'+i;
+            labela.innerHTML=`Zadaci vjezbe${i+1}`;
+            labela.for='z'+i;
             DOMelementDIVauFormi.appendChild(labela);
             var input = document.createElement("input");
+            input.id = 'z'+i;
             input.type = "number";
-            input.name = "vjezba" + i;
+            input.name = "z" + i;
             input.classList.add('form-control');
             input.classList.add('col-md-3');
             input.classList.add('mt-2');
+            input.classList.add('zadatak');
+            input.value = 4;
             input.required = true;
             DOMelementDIVauFormi.appendChild(input);
             var div = document.createElement('div');
@@ -23,7 +26,24 @@ let VjezbeAjax = (function(){
         }
     }
     let posaljiPodatke = function(vjezbeObjekat,callbackFja){
+        var ajax = new XMLHttpRequest();
+        ajax.open("POST","http://localhost:3000/vjezbe",true);
+        ajax.setRequestHeader("Content-Type", "application/json");
+        
+        ajax.onreadystatechange = function() {
+            if (ajax.readyState == 4 && ajax.status == 200){
+                var rezultat = JSON.parse(ajax.responseText);
+                if (rezultat.status=="error")
+                {
+                    callbackFja(rezultat.data, null);
+                }
+                callbackFja(null, rezultat);
+            }
+            else if (ajax.readyState == 4)
+                callbackFja(ajax.statusText,null);
+        }
 
+        ajax.send(JSON.stringify(vjezbeObjekat));
     }
 
     return{
